82851166c6b9932f849d638069470b52ec0293fd
Stryker Foxx <hokaygames358@gmail.com>
2024-10-27 14:34:15 -0600

    frontend done

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.imgur.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+      ],
+    },
+  }
+// domains: [`localhost`, 'i.imgur.com', 'images.unsplash.com']
+    
+
+export default nextConfig;
